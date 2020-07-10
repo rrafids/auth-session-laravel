382e975dafdb3dadb39e5a848727fd8aa4403654
rrafids <55825072+rrafids@users.noreply.github.com>
2020-07-11 01:49:37 +0700

    Update header.blade.php
    kembalikan ke tidak bold

--- resources/views/templates/users/header.blade.php
+++ resources/views/templates/users/header.blade.php
@@ -1,6 +1,6 @@
 <div class="container">
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="/"><b>Festival Makan</b></a>
+        <a class="navbar-brand" href="/">Festival Makan</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
